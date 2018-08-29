7462ec2ee4abc8062727add08c07c4a4e728d38b
Darren Forsythe <git@darrenforsythe.com>
2018-08-29 18:17:59 +0100

    Fix tests

--- src/test/java/fukoka/testing/CucumberWithSerenityTest.java
+++ src/test/java/fukoka/testing/CucumberWithSerenityTest.java
@@ -3,7 +3,12 @@ package fukoka.testing;
 
 import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(plugin =
@@ -18,5 +23,11 @@ import org.junit.runner.RunWith;
          strict = true,
          tags = "not @ignore"
 )
+@ContextConfiguration(classes = TestConfiguration.class)
 public class CucumberWithSerenityTest {
+    @ClassRule
+    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
+
+    @Rule
+    public final SpringMethodRule springMethodRule = new SpringMethodRule();
 }
